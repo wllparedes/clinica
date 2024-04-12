b468dbb5ad66e45b03096328b7e25be174149b4a
Walin Paredes <walinparedes3010@gmail.com>
2024-04-12 00:10:06 -0500

    Add favicon to admin and clinic layouts

--- resources/views/admin/common/layouts/app.blade.php
+++ resources/views/admin/common/layouts/app.blade.php
@@ -8,6 +8,8 @@
 
     <title> Admin | Clinica </title>
 
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/clinic/logo.png') }}">
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
