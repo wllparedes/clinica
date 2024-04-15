6f30a19100190bb13943bb383586d5c108e787d5
Walin Paredes <walinparedes3010@gmail.com>
2024-04-14 19:23:05 -0500

    Update footer and sidebar styles

--- resources/views/clinic/common/partials/footer.blade.php
+++ resources/views/clinic/common/partials/footer.blade.php
@@ -1,8 +1,8 @@
-<footer class="bg-cyan-950 dark:bg-gray-800 p-4 sm:ml-64">
+<footer class=" dark:bg-gray-800 p-4 sm:ml-64 bg-slate-800">
     <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-center">
         <span class="text-sm text-white sm:text-center dark:text-gray-400">
             &copy; {{ getCurrentYear() }}
-            <a href="#" class="hover:underline">ADP Clinica</a>. {{ __('All rights reserved.') }}
+            <a href="/" class="hover:underline">ADP Clinica</a>. {{ __('All rights reserved.') }}
         </span>
     </div>
 </footer>
--- resources/views/clinic/common/partials/sidebar.blade.php
+++ resources/views/clinic/common/partials/sidebar.blade.php
@@ -1,34 +1,17 @@
 <aside id="logo-sidebar"
-    class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full  border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+    class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700 bg-secondary-900"
     aria-label="Sidebar">
-    <div class="h-full px-3 pb-4 overflow-y-auto dark:bg-gray-800 soft-scrollbar">
+    <div class="h-full px-3 pb-4 overflow-y-auto
+    dark:bg-gray-800">
         <ul class="space-y-2 font-medium">
-            <li>
-                <a href="{{ route('clinic.dashboard') }}"
-                    class="{{ setActive('clinic.dashboard') }} flex items-center p-2 rounded-lg dark:text-white hover:bg-gray-300 dark:hover:bg-gray-700 group text-black hover:text-black">
-                    <x-icon name="home" class="w-5 h-5" />
-                    <span class="ms-3"> {{ __('Home') }} </span>
-                </a>
-            </li>
+
+            <x-sidebar-link route="clinic.dashboard" icon="home" label="Home" />
 
             @can('denyDoctor')
-                <li>
-                    <a href="{{ route('clinic.appointments') }}"
-                        class="{{ setActive('clinic.appointments') }} flex items-center p-2 rounded-lg dark:text-white hover:bg-gray-300 dark:hover:bg-gray-700 group text-black hover:text-black">
-                        <x-icon name="annotation" class="w-5 h-5" />
-                        <span class="flex-1 ms-3 whitespace-nowrap">{{ __('Appointment requests') }}</span>
-                    </a>
-                </li>
+                <x-sidebar-link route="clinic.appointments" icon="annotation" label="Appointment requests" />
             @endcan
 
-            <li>
-                <a href="{{ route('clinic.medical-requests') }}"
-                    class="{{ setActive('clinic.medical-requests') }} flex items-center p-2 rounded-lg dark:text-white hover:bg-gray-300 dark:hover:bg-gray-700 group text-black hover:text-black">
-                    <x-icon name="calendar" class="w-5 h-5" />
-                    <span class="flex-1 ms-3 whitespace-nowrap">{{ __('Medical appointments') }} </span>
-                </a>
-            </li>
-
+            <x-sidebar-link route="clinic.medical-requests" icon="calendar" label="Medical appointments" />
 
         </ul>
     </div>
