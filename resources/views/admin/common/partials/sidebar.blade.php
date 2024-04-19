319b753ddd24a95b0c9ba648b03c63a68d63af41
Walin Paredes <walinparedes3010@gmail.com>
2024-04-18 20:27:13 -0500

    Refactor sidebar and navigation menu components

--- resources/views/admin/common/partials/sidebar.blade.php
+++ resources/views/admin/common/partials/sidebar.blade.php
@@ -1,24 +1,16 @@
-<aside id="logo-sidebar"
-    class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700 bg-secondary-900"
+<aside id="logo-sidebar" x-data="{ open: window.innerWidth > 768 }" x-on:open-sidebar.window="open = !open"
+    x-on:resize.window="open = window.innerWidth > 768" :class="{ 'translate-x-0': open, '-translate-x-full': !open }"
+    class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform duration-200 ease-in-out transform dark:bg-gray-800 dark:border-gray-700 bg-secondary-900"
     aria-label="Sidebar">
-    <div class="h-full px-3 pb-4 overflow-y-auto
-    dark:bg-gray-800">
+    <div class="h-full px-3 pb-4 overflow-y-auto dark:bg-gray-800">
         <ul class="space-y-2 font-medium">
-
             <x-sidebar-link route="admin.dashboard" icon="home" label="Home" />
-
             <x-sidebar-link route="admin.staff" icon="users" label="Staff" />
-
             <x-sidebar-link route="admin.patients" icon="user" label="Patients" />
-
             <x-sidebar-link route="admin.appointments" icon="annotation" label="Appointment requests" />
-
             <x-sidebar-link route="admin.medical-requests" icon="calendar" label="Medical appointments" />
-
             <x-sidebar-link route="admin.products" icon="puzzle" label="Products" />
-
             <x-sidebar-link route="admin.specialties" icon="beaker" label="Specialties" />
-
         </ul>
     </div>
 </aside>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -2,12 +2,13 @@
     <div class="px-3 py-3 lg:px-5 lg:pl-3">
         <div class="flex items-center justify-between">
             <div class="flex items-center justify-start rtl:justify-end">
-                <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar"
-                    type="button"
+
+                <button type="button" x-on:click="$dispatch('open-sidebar')"
                     class="inline-flex items-center p-2 text-sm text-white rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 hover:text-gray-400 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
                     <span class="sr-only">Open sidebar</span>
                     <x-icon name="menu" class="w-6 h-6 text-slate-700" />
                 </button>
+
                 <a href="{{ route('admin.dashboard') }}" class="flex ms-2 md:me-24">
                     <img src="{{ asset('images/clinic/adp.png') }}" class="h-10 me-3" alt="APD Technology" />
                 </a>
@@ -16,6 +17,7 @@
                 <div class="flex items-center ms-3 gap-3">
 
                     {{-- notifications --}}
+
                     <livewire:view-notifications />
 
                     {{-- profile --}}
